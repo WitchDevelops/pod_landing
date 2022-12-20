5beb715952b608618634edfad4f1a10aad02d179
W1ckedW1tch <112077394+WitchDevelops@users.noreply.github.com>
2022-12-20 12:45:58 +0100

    Change form validation

--- emailValidation.js
+++ emailValidation.js
@@ -1,36 +1,23 @@
-const form = document.querySelector("form");
-const email = document.getElementById("email");
-const emailError = document.querySelector("#email + span.error");
+const form = document.getElementById('subscribe');
 
-email.addEventListener("input", (event) => {
-    if (email.validity.valid) {
-  //if email is valid, there is no message
-  email.Error.textContent = "";
-  email.Error.className ="error";
+let validation = document.createElement('span');
+validation.classList.add('invalid');
+validation.innerText = 'Oops! Please check your email';
+
+function ValidateEmail(input) {
+  let validRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (input.match(validRegex)) {
+    alert('Valid email address!');
   } else {
-    showError();
+    form.appendChild(validation);
   }
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = document.getElementById('email').value;
+  console.log(email);
+  ValidateEmail(email);
 });
-
-form.addEventListener("submit", (event) => {
-    // if the email field is valid, we let the form submit
-    if (!email.validity.valid) {
-      // If it isn't, we display an appropriate error message
-      showError();
-// Then we prevent the form from being sent by canceling the event
-event.preventDefault();
-    }
-  });
-
-  function showError() {
-    if (email.validity.valueMissing) {
-      // If the field is empty, display the following error message.
-      emailError.textContent = "Oops! Please check your email";
-    } else if (email.validity.typeMismatch) {
-      // If the field doesn't contain an email address, display the following error message.
-      emailError.textContent = "Oops! Please check your email";
-    }
-  
-    // Set the styling appropriately
-    emailError.className = "error active";
-  }
